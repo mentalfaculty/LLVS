ab82b0c295575255a7c39f4531a411193fcd860e
Drew McCormack <drewmccormack@mac.com>
2018-11-04 19:18:01 +0100

    Renamed llvs to LLVS

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "llvs",
+    name: "LLVS",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "llvs",
-            targets: ["llvs"]),
+            name: "LLVS",
+            targets: ["LLVS"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "llvs",
+            name: "LLVS",
             dependencies: []),
         .testTarget(
-            name: "llvsTests",
-            dependencies: ["llvs"]),
+            name: "LLVSTests",
+            dependencies: ["LLVS"]),
     ]
 )
