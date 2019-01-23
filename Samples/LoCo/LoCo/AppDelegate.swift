c8f0dbf0479376545f73d8f390aafb1c2540e45e
Drew McCormack <drewmccormack@mac.com>
2019-01-23 20:27:49 +0100

    Added code to create store and ContactBook.

--- Samples/LoCo/LoCo/AppDelegate.swift
+++ Samples/LoCo/LoCo/AppDelegate.swift
@@ -7,18 +7,53 @@
 //
 
 import UIKit
+import LLVS
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
 
     var window: UIWindow?
-
+    
+    private let storeVersionKey = "storeVersion"
+    
+    lazy var rootStoreDirectory: URL = {
+        let docDir = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+        let rootDir = docDir.appendingPathComponent("ContactBook")
+        try? FileManager.default.createDirectory(at: rootDir, withIntermediateDirectories: true, attributes: nil)
+        return rootDir
+    }()
+    
+    lazy var store: Store = {
+        return try! Store(rootDirectoryURL: rootStoreDirectory)
+    }()
+    
+    lazy var contactBook: ContactBook = {
+        let book: ContactBook
+        let versionIdentifier: Version.Identifier
+        if let versionString = UserDefaults.standard.string(forKey: storeVersionKey) {
+            versionIdentifier = .init(versionString)
+            book = try! ContactBook(prevailingAt: versionIdentifier, loadingFrom: store)
+        } else if let version = store.history.mostRecentHead?.identifier {
+            versionIdentifier = version
+            book = try! ContactBook(prevailingAt: version, loadingFrom: store)
+        } else {
+            book = try! ContactBook(creatingIn: store)
+            versionIdentifier = book.currentVersion
+        }
+        UserDefaults.standard.set(versionIdentifier.identifierString, forKey: storeVersionKey)
+        return book
+    }()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        NotificationCenter.default.addObserver(forName: .contactBookVersionDidChange, object: contactBook, queue: nil) { notif in
+            UserDefaults.standard.set(self.contactBook.currentVersion.identifierString, forKey: self.storeVersionKey)
+        }
+        
         let splitViewController = window!.rootViewController as! UISplitViewController
         let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as! UINavigationController
         navigationController.topViewController!.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem
         splitViewController.delegate = self
+        
         return true
     }
 
--- Samples/LoCo/LoCo/ContactBook.swift
+++ Samples/LoCo/LoCo/ContactBook.swift
@@ -13,46 +13,67 @@ extension Notification.Name {
     static let contactBookVersionDidChange = Notification.Name("contactBookVersionDidChange")
 }
 
-class ContactBook {
+final class ContactBook {
     
     let store: Store
     private(set) var contacts: [Fault<Contact>] = []
     
+    private static let sharedIdentifier: Value.Identifier = .init("ContactBook")
+    
     var currentVersion: Version.Identifier {
         didSet {
+            try! fetchContacts()
             NotificationCenter.default.post(name: .contactBookVersionDidChange, object: self)
         }
     }
     
-    required init?(_ valueIdentifier: Value.Identifier, prevailingAt version: Version.Identifier, loadingFrom store: Store) throws {
+    init(prevailingAt version: Version.Identifier, loadingFrom store: Store) throws {
         self.store = store
-        self.currentVersion = version
+        currentVersion = version
         try fetchContacts()
+    }
+    
+    init(creatingIn store: Store) throws {
+        self.store = store
+        currentVersion = try store.addVersion(basedOnPredecessor: nil, storing: []).identifier
     }
     
     func add(_ contact: Contact) throws {
         var valueIdentifiers = contacts.map { $0.valueIdentifier }
         valueIdentifiers.append(contact.valueIdentifier)
-        let data = try JSONSerialization.data(withJSONObject: valueIdentifiers, options: [])
-        let newValue = Value(identifier: .init("ContactBook"), version: nil, data: data)
-        let update: Value.Change = .update(newValue)
         
         let contact = Contact()
         let insertChanges = try contact.changesSinceLoad(from: store)
-        
-        currentVersion = try store.addVersion(basedOnPredecessor: currentVersion, storing: insertChanges + [update]).identifier
+        let changes = insertChanges + [try updateContactsChange(withContactIdentifiers: valueIdentifiers)]
+            
+        currentVersion = try store.addVersion(basedOnPredecessor: currentVersion, storing: changes).identifier
         
         try fetchContacts()
     }
     
-    func fetchContacts() throws {
-        let data = try store.value(.init("ContactBook"), prevailingAt: currentVersion)!.data
+    func delete(_ contact: Contact) throws {
+        var valueIdentifiers = contacts.map { $0.valueIdentifier }
+        let index = valueIdentifiers.firstIndex(of: contact.valueIdentifier)!
+        valueIdentifiers.remove(at: index)
+        
+        let removal: Value.Change = .remove(contact.valueIdentifier)
+        let changes = [removal, try updateContactsChange(withContactIdentifiers: valueIdentifiers)]
+        currentVersion = try store.addVersion(basedOnPredecessor: currentVersion, storing: changes).identifier
+        
+        try fetchContacts()
+    }
+    
+    private func fetchContacts() throws {
+        let data = try store.value(ContactBook.sharedIdentifier, prevailingAt: currentVersion)!.data
         let contactIds = try JSONSerialization.jsonObject(with: data, options: []) as! [Value.Identifier]
         self.contacts = contactIds.map { Fault<Contact>($0, prevailingAtVersion: currentVersion, in: store) }
     }
     
-    func delete(_ contact: Contact) throws {
-        
+    private func updateContactsChange(withContactIdentifiers valueIdentifiers: [Value.Identifier]) throws -> Value.Change {
+        let data = try JSONSerialization.data(withJSONObject: valueIdentifiers, options: [])
+        let newValue = Value(identifier: ContactBook.sharedIdentifier, version: nil, data: data)
+        let update: Value.Change = .update(newValue)
+        return update
     }
 
 }
