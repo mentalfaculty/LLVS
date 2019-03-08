c2160b1b8649e788216e88fb5ec9a110f7a2b667
Drew McCormack <drewmccormack@mac.com>
2019-03-08 20:29:47 +0100

    Added merge policy that favors most recent change in any conflict.

--- Sources/LLVS/Merge.swift
+++ Sources/LLVS/Merge.swift
@@ -54,3 +54,40 @@ class MostRecentBranchFavoringArbiter: MergeArbiter {
     }
     
 }
+
+/// Favors the most recent change on a conflict by conflict basis.
+/// Will pick an update over a removal, regardless of recency.
+class MostRecentChangeFavoringArbiter: MergeArbiter {
+    
+    func changes(toResolve merge: Merge, in store: Store) throws -> [Value.Change] {
+        let v = merge.versions
+        var changes: [Value.Change] = []
+        for (valueId, fork) in merge.forksByValueIdentifier {
+            switch fork {
+            case let .removedAndUpdated(removeBranch):
+                let favoredVersion = removeBranch.opposite == .first ? v.first : v.second
+                let value = try store.value(valueId, prevailingAt: favoredVersion.identifier)!
+                changes.append(.preserve(value.reference!))
+            case .twiceInserted, .twiceUpdated:
+                let value1 = try store.value(valueId, prevailingAt: v.first.identifier)!
+                var version1: Version!
+                store.queryHistory { version1 = $0.version(identifiedBy: value1.version!) }
+                
+                let value2 = try store.value(valueId, prevailingAt: v.second.identifier)!
+                var version2: Version!
+                store.queryHistory { version2 = $0.version(identifiedBy: value2.version!) }
+                
+                if version1.timestamp >= version2.timestamp {
+                    changes.append(.preserve(value1.reference!))
+                } else {
+                    changes.append(.preserve(value2.reference!))
+                }
+            case .inserted, .removed, .updated, .twiceRemoved:
+                break
+            }
+        }
+        return changes
+    }
+    
+}
+
