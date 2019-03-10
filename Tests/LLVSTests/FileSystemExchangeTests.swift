41da180f4a2dc0feef38fa7a70c5dbb6a9110c8f
Drew McCormack <drewmccormack@mac.com>
2019-03-10 20:16:50 +0100

    Started adding FileSystemExchange tests.

--- Tests/LLVSTests/FileSystemExchangeTests.swift
+++ Tests/LLVSTests/FileSystemExchangeTests.swift
@@ -6,27 +6,61 @@
 //
 
 import XCTest
+import Foundation
+@testable import LLVS
 
 class FileSystemExchangeTests: XCTestCase {
 
+    let fm = FileManager.default
+    
+    var store1, store2: Store!
+    var rootURL1, rootURL2: URL!
+    var exchangeURL: URL!
+    var exchange1, exchange2: FileSystemExchange!
+    
+    var recentChangeArbiter: MostRecentChangeFavoringArbiter!
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        rootURL1 = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(UUID().uuidString)
+        rootURL2 = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(UUID().uuidString)
+        store1 = try! Store(rootDirectoryURL: rootURL1)
+        store2 = try! Store(rootDirectoryURL: rootURL2)
+        recentChangeArbiter = MostRecentChangeFavoringArbiter()
+        exchangeURL = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(UUID().uuidString)
+        exchange1 = FileSystemExchange(rootDirectoryURL: exchangeURL, store: store1)
+        exchange2 = FileSystemExchange(rootDirectoryURL: exchangeURL, store: store2)
     }
-
+    
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try? FileManager.default.removeItem(at: rootURL1)
+        try? FileManager.default.removeItem(at: rootURL2)
+        try? FileManager.default.removeItem(at: exchangeURL)
+        super.tearDown()
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    private func value(_ identifier: String, stringData: String) -> Value {
+        return Value(identifier: .init(identifier), version: nil, data: stringData.data(using: .utf8)!)
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testExchangeDirContainsFiles() {
+        let val = value("CDEFGH", stringData: "Origin")
+        let ver = try! store1.addVersion(basedOnPredecessor: nil, storing: [.insert(val)])
+        XCTAssertEqual(try! fm.contentsOfDirectory(at: exchangeURL.appendingPathComponent("changes"), includingPropertiesForKeys: nil, options: [.skipsHiddenFiles]).count, 0)
+        XCTAssertEqual(try! fm.contentsOfDirectory(at: exchangeURL.appendingPathComponent("versions"), includingPropertiesForKeys: nil, options: [.skipsHiddenFiles]).count, 0)
+        let expect = self.expectation(description: "Send")
+        exchange1.send { result in
+            if case let .success(versionIds) = result {
+                XCTAssert(versionIds.contains(ver.identifier))
+            } else {
+                XCTFail()
+            }
+            expect.fulfill()
         }
+        wait(for: [expect], timeout: 1.0)
     }
 
+    static var allTests = [
+        ("testExchangeDirContainsFiles", testExchangeDirContainsFiles),
+    ]
 }
