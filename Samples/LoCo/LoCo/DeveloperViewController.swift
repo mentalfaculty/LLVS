97bb29e0e5357cc977deeb7f2aee86285668e446
Drew McCormack <drewmccormack@mac.com>
2019-05-22 20:46:39 +0200

    Added a Developer view controller, but not yet in use.

---
+++ Samples/LoCo/LoCo/DeveloperViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DeveloperViewController.swift
+//  LoCo
+//
+//  Created by Drew McCormack on 22/05/2019.
+//  Copyright © 2019 The Mental Faculty B.V. All rights reserved.
+//
+
+import UIKit
+
+class DeveloperViewController: UIViewController {
+
+    @IBAction func dismiss(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
